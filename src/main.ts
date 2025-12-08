9bc6139b809d78c9e232e5e006358b91e6be85cd
Mochammad Syahrul Kurniawan <mochrul.kurniawan@gmail.com>
2025-12-08 08:57:36 +0800

    fix: update API base URL and clean up unused axios import

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,3 @@
-import axios from 'axios';
 import { registerSW } from 'virtual:pwa-register';
 import { createApp } from 'vue';
 import App from './App.vue';
@@ -7,7 +6,5 @@ import router from './router';
 registerSW({
   immediate: true,
 });
-
-axios.defaults.withCredentials = true;
 
 createApp(App).use(router).mount('#app');
