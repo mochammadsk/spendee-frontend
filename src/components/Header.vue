6961c54d277c0bd8f52a4b8d92ee42907fc36f4b
Mochammad Syahrul Kurniawan <mochrul.kurniawan@gmail.com>
2025-12-13 10:16:06 +0800

    fix: update navbar styling same like sidebar

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,113 +1,48 @@
 <template>
-  <div
-    class="space-y-4 rounded-b-3xl bg-linear-to-b from-sky-500 via-sky-700 to-indigo-900 p-4 pb-6"
+  <header
+    class="flex items-center justify-between bg-linear-to-r from-[#143D60] to-[#17594A] px-4 text-white transition-all duration-300"
+    :class="collapsed ? 'h-12' : 'h-16'"
   >
-    <div class="flex items-center justify-between">
-      <div class="grid text-white">
-        <h1 class="text-xl font-bold">Halo, {{ name }}</h1>
-        <div class="flex items-center gap-1 text-sm">
-          <Calendar class="h-3 w-3" />
-          <span>{{ today }}</span>
-        </div>
-      </div>
-      <div class="text-white">
-        <Bell class="h-5 w-5" />
-      </div>
+    <div class="flex items-center gap-3">
+      <img
+        src="/icons/icon.webp"
+        alt="logo"
+        class="transition-all duration-300"
+        :class="collapsed ? 'w-8' : 'w-10'"
+      />
+
+      <h1
+        v-if="!collapsed"
+        class="text-xl font-semibold transition-all duration-300"
+      >
+        Spendee
+      </h1>
     </div>
 
-    <div
-      class="grid items-center space-y-4 rounded-xl border border-gray-100/70 bg-gray-100/20 p-4 text-white"
-    >
-      <div class="grid">
-        <div class="flex justify-between">
-          <h1 class="text-md">Total Saldo</h1>
-          <button
-            type="button"
-            class="cursor-pointer text-white duration-200 active:scale-95"
-            @click="toggleBalance"
-            :title="showBalance ? 'Hide Balance' : 'Show Balance'"
-          >
-            <Eye v-if="!showBalance" class="h-5 w-5" />
-            <EyeOff v-else class="h-5 w-5" />
-          </button>
-        </div>
-        <p class="text-2xl font-semibold">{{ balance }}</p>
+    <!-- RIGHT: Date + Notification -->
+    <div class="flex items-center gap-4">
+      <div class="border-r border-white/50 pr-4">
+        <span class="text-sm">{{ today }}</span>
       </div>
 
-      <div class="flex items-center justify-between">
-        <div class="flex gap-2 p-2">
-          <div class="rounded-lg bg-green-500/20 p-2 text-green-500">
-            <TrendingDown class="h-6 w-6" />
-          </div>
-
-          <div class="grid">
-            <h2 class="text-sm">Pemasukan</h2>
-            <span class="text-xs">{{ balance }}</span>
-          </div>
-        </div>
-
-        <div class="flex gap-2 p-2">
-          <div class="rounded-lg bg-red-500/20 p-2 text-red-500">
-            <TrendingUp class="h-6 w-6" />
-          </div>
-
-          <div class="grid">
-            <h2 class="text-sm">Pengeluaran</h2>
-            <span class="text-xs">{{ balance }}</span>
-          </div>
-        </div>
-      </div>
+      <button
+        class="flex cursor-pointer rounded-full border border-white p-1.5 hover:scale-105 active:scale-95"
+      >
+        <Bell class="h-4 w-4" />
+      </button>
     </div>
-  </div>
+  </header>
 </template>
 
 <script setup>
-import Cookies from 'js-cookie';
-import { jwtDecode } from 'jwt-decode';
-import {
-  Bell,
-  Calendar,
-  Eye,
-  EyeOff,
-  TrendingDown,
-  TrendingUp,
-} from 'lucide-vue-next';
-import { computed, ref } from 'vue';
+import { Bell } from 'lucide-vue-next';
 
-const name = ref('');
-const token = Cookies.get('token');
-if (token) {
-  try {
-    const decoded = jwtDecode(token);
-    name.value = decoded.full_name;
-  } catch (err) {
-    console.error('Token invalid:', err);
-  }
-}
-
-const showBalance = ref(false);
-function toggleBalance() {
-  showBalance.value = !showBalance.value;
-}
-
-function formatRupiah(amount) {
-  if (typeof amount !== 'number') return amount;
-  return amount.toLocaleString('id-ID', {
-    style: 'currency',
-    currency: 'IDR',
-    maximumFractionDigits: 0,
-  });
-}
-
-const rawBalance = ref(100000);
-const balance = computed(() => {
-  if (showBalance.value) {
-    return formatRupiah(rawBalance.value);
-  }
-  return '••••••';
+const props = defineProps({
+  collapsed: Boolean,
 });
 
 const today = new Date().toLocaleDateString('id-ID', {
+  weekday: 'long',
   day: 'numeric',
   month: 'long',
   year: 'numeric',
