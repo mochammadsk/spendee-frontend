3ad96bc85ede43529d3e075e114b857ede771d98
Mochammad Syahrul Kurniawan <mochrul.kurniawan@gmail.com>
2025-12-13 19:38:25 +0800

    fix: add toast for interceptors error and set timeout to 15s

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,9 +1,27 @@
 import axios from 'axios';
+import { useToast } from 'vue-toast-notification';
 
 const api = axios.create({
   baseURL: 'https://spendee-api.vercel.app/api',
   withCredentials: true,
-  timeout: 30000,
+  timeout: 15000,
 });
+
+// Interceptors
+const toast = useToast();
+api.interceptors.response.use(
+  (res) => res,
+  (err) => {
+    if (!err.response) {
+      toast.error('Terjadi kesalahan. Silakan coba lagi.', {
+        position: 'top',
+        duration: 2500,
+      });
+    } else {
+      //
+    }
+    return Promise.reject(err);
+  },
+);
 
 export default api;
